c45ae4a7d8a7e637ef7e344139b727ab12e9cca7
Inov Yomanovian <yomanovian@gmail.com>
2015-04-21 12:20:49 +0700

    Add macro to identify apple mac os x as unix

--- nwg_common_socket_include.h
+++ nwg_common_socket_include.h
@@ -1,6 +1,10 @@
 #ifndef NWG_COMMON_SOCKET_INCLUDE_H_
 #define NWG_COMMON_SOCKET_INCLUDE_H_
 
+#if !defined(__unix__) && (defined(__APPLE__) && defined(__MACH__))
+#define __unix__ 1
+#endif
+
 #ifdef __unix__
 #include <netinet/in.h>
 #include <sys/socket.h>
