030883e067e3230993f9f1a4e74a1b6f87e7caca
Inov Yomanovian <yomanovian@gmail.com>
2015-08-05 17:15:32 +0700

    Update examples/httpserverv3.cc

--- examples/httpserverv3.cc
+++ examples/httpserverv3.cc
@@ -12,7 +12,7 @@
 #define READBUFFSIZE SZ_4MB
 
 #ifndef SILENT
-#define SILENT 1
+#define SILENT 0
 #endif
 
 #if SILENT
@@ -26,25 +26,93 @@ using boost::regex_match;
 using boost::regex_replace;
 using boost::smatch;
 
-using namespace boost::filesystem;
-
 static int numReq = 0;
-static path workingPath;
-
-struct SessionState {
-    int reqNo             = 0;
-    size_t nwritten       = 0;
-    size_t length         = 0;
-    std::ifstream *is     = nullptr;
-    bool readAndWriteFile = false;
-};
-
-#define STATE "STATE"
-#define GETMSG(obj) dynamic_cast<Nwg::MessageBuffer &>(obj)
-#define GETSTATE(session) session.get<SessionState>(STATE)
 
 #define PATTERN "([a-zA-Z]+) (/[0-9a-zA-Z\\-_,.;:'\"\\[\\]\\(\\)+=!@#$%^&*<>/?~`{}|]*) (HTTP/\\d\\.\\d)(\r|)$"
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Protocol Codec
+////////////////////////////////////////////////////////////////////////////////////////////////////
+class HttpRequest : public Nwg::MessageBuffer
+{
+public:
+    void setMethod(const std::string &method) { _method = method; }
+    void setUrl(const std::string &url) { _url = url; }
+    void setHttpVersion(const std::string &httpVersion) { _httpVersion = httpVersion; }
+    void requestOk() { _requestOk = true; }
+    std::string &getMethod() { return _method; }
+    std::string &getUrl() { return _url; }
+    std::string &getHttpVersion() { return _httpVersion; }
+    bool isRequestOk() { return _requestOk; }
+private:
+    std::string _method;
+    std::string _url;
+    std::string _httpVersion;
+    bool _requestOk = false;
+};
+
+class HttpResponse : public Nwg::MessageBuffer
+{
+public:
+    void setStatusCode(const std::string &statusCode) { _statusCode = statusCode; }
+    void setContentType(const std::string &contentType) { _contentType = contentType; }
+    void setConnection(const std::string &connection) { _connection = connection; }
+    void setBody(const std::string &body) { _body = body; }
+    std::string &getStatusCode() { return _statusCode; }
+    std::string &getContentType() { return _contentType; }
+    std::string &getConnection() { return _connection; }
+    std::string &getBody() { return _body; }
+private:
+    std::string _statusCode = "200 OK";
+    std::string _contentType = "text/html";
+    std::string _connection = "close";
+    std::string _body = "";
+};
+
+class HttpProtocolCodec : public Nwg::ProtocolCodec
+{
+public:
+    void transformUp(Nwg::MessageBuffer *in, Nwg::MessageBuffer **out) const
+    {
+        HttpRequest *req = new HttpRequest();
+
+        // HTTP Header's First Line
+        std::string h1 = in->sreadUntil('\n');
+
+        smatch what;
+        if (regex_match(h1, what, regex(PATTERN))) {
+            req->setMethod(what[1].str());
+            req->setUrl(what[2].str());
+            req->setHttpVersion(what[3].str());
+            req->requestOk();
+        }
+
+        *out = req;
+    }
+
+    void transformDown(Nwg::MessageBuffer *in, Nwg::MessageBuffer *out) const
+    {
+        HttpResponse *res = dynamic_cast<HttpResponse *>(in);
+
+        out->reset();
+        out->put("HTTP/1.1 " + res->getStatusCode() + "\r\n");
+        out->put("Content-Type: " + res->getContentType() + "\r\n");
+
+        if (res->getConnection() != "") {
+            out->put("Connection: " + res->getConnection() + "\r\n");
+        }
+
+        out->put("\r\n");
+        out->put(res->getBody());
+        out->flip();
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Http Handler
+////////////////////////////////////////////////////////////////////////////////////////////////////
 class HttpHandler : public Nwg::Handler
 {
 private:
@@ -63,219 +131,68 @@ private:
 public:
     void sessionOpened(Nwg::Session &session)
     {
-        std::shared_ptr<SessionState> state(new SessionState());
-        state->reqNo = ++numReq;
+        int reqNo = ++numReq;
+        session.put<int>("req_no", std::make_shared<int>(reqNo));
 
-        session.put<SessionState>(STATE, state);
+        _printf("|||| SESSION OPENED #%d\n", reqNo);
     }
 
-    void messageReceived(Nwg::Session &session, Nwg::MessageBuffer &obj)
+    void messageReceived(Nwg::Session &session, Nwg::MessageBuffer &msg)
     {
-        Nwg::MessageBuffer &msg = GETMSG(obj);
-        SessionState &state  = GETSTATE(session);
+        // Got request
+        HttpRequest &req = dynamic_cast<HttpRequest &>(msg);
 
-        if (session.stillReading) {
-            session.close();
-        }
-
-        std::shared_ptr<Nwg::MessageBuffer> out(new Nwg::MessageBuffer(BUFFSIZE));
-
-        std::string h1 = msg.sreadUntil('\n');
-
-        _printf("==== REQUEST RECEIVED %d ====\n", state.reqNo);
-        _printf("%s\n", h1.c_str());
-
-        smatch what;
-        std::string url = "";
-        bool requestOk = false;
-
-        if (regex_match(h1, what, pattern)) {
-            _printf("Method: %s\n", what[1].str().c_str());
-            _printf("URL: %s\n", what[2].str().c_str());
-            _printf("HTTP Version: %s\n", what[3].str().c_str());
-
-            url = decodeUrl(what[2].str());
-            requestOk = true;
+        _printf("|||| REQUEST RECEIVED #%d\n", session.get<int>("req_no"));
+        if (req.isRequestOk()) {
+            _printf("| Method: %s\n", req.getMethod().c_str());
+            _printf("| URL: %s\n", req.getUrl().c_str());
+            _printf("| HTTP Version: %s\n", req.getHttpVersion().c_str());
         } else {
-            _printf("MALFORMED REQUEST.\n");
+            _printf("| MALFORMED REQUEST.\n");
         }
 
-        _printf("==== ==== ====\n");
+        // Build response
+        std::shared_ptr<HttpResponse> res(new HttpResponse());
 
-        path urlPath = workingPath;
-        urlPath += url;
+        if (req.isRequestOk()) {
+            std::stringstream body;
 
-        /* request is okay and it is a directory */
-        if (requestOk && is_directory(urlPath) && ((int) url.find("..") == -1))
-        {
-            std::string sout;
-            sout.reserve(BUFFSIZE);
+            body << "<h1>Hi.</h1>\n";
+            body << "<ul>\n";
+            body << "<li>Method: " << req.getMethod() << "</li>\n";
+            body << "<li>URL: " << req.getUrl() << "</li>\n";
+            body << "<li>HTTP Version: " << req.getHttpVersion() << "</li>\n";
+            body << "<li>Req. Number #" << session.get<int>("req_no") << "</li>\n";
+            body << "</ul>\n";
 
-            path p = urlPath;
-            if (is_directory(p)) {
-                typedef std::vector<path> vec;
-                vec v;
-
-                std::copy(directory_iterator(p), directory_iterator(), std::back_inserter<vec>(v));
-                sort(v.begin(), v.end());
-
-                for (vec::const_iterator it(v.begin()), it_end(v.end()); it != it_end; ++it) {
-                    if (is_directory(*it)) {
-                        sout += "  <li><a href=\"" + it->filename().string() + "/\">" + it->filename().string() + "/</li>\n";
-                    } else {
-                        sout += "  <li><a href=\"" + it->filename().string() + "\">" + it->filename().string() + "</li>\n";
-                    }
-                }
-            }
-
-            out->put("HTTP/1.1 200 OK\r\n");
-            out->put("Content-Type: text/html\r\n");
-            out->put("Connection: close\r\n");
-            out->put("\r\n");
-
-            out->put("<h1>Listing of " + urlPath.string() + "</h1>\n");
-            out->put("<ul>\n");
-            out->put(sout);
-            out->put("</ul>\n");
-
-            out->flip();
-
-            session.write(out);
-            return;
+            res->setContentType("text/html");
+            res->setBody(body.str());
+        } else {
+            res->setStatusCode("400 BAD REQUEST");
+            res->setContentType("text/html");
+            res->setBody("<h1>400 BAD REQUEST</h1>\n");
         }
 
-        /* request is okay and it is likely a regular file */
-        else if (requestOk)
-        {
-            url = url.substr(1);
-            path p(url);
-
-            if (is_regular_file(p))
-            {
-                std::ifstream *is = new std::ifstream(p.string(), std::ios::binary);
-                is->seekg(0, is->end);
-                int length = is->tellg();
-                is->seekg(0, is->beg);
-
-                state.readAndWriteFile = true;
-                state.length = length;
-                state.nwritten = 0;
-                state.is = is;
-
-                _printf("==== DATA TO BE WRITTEN: %d ====\n", length);
-
-                out->put("HTTP/1.1 200 OK\r\n");
-                out->put("Content-Type: text/plain\r\n");
-                out->put("Connection: close\r\n");
-                out->put("Content-Length: " + std::to_string(length) + "\r\n");
-                out->put("\r\n");
-
-                out->flip();
-
-                /* send the header first */
-                /* will continue reading the file and sending it at messageSent() */
-
-                session.write(out);
-                return;
-            }
-
-            /* neither regular file nor directory, let's assume it is not found */
-            else
-            {
-                out->put("HTTP/1.1 404 NOT FOUND\r\n");
-                out->put("Content-Type: text/html\r\n");
-                out->put("Connection: close\r\n");
-                out->put("\r\n");
-
-                out->put("<h1>404 NOT FOUND</h1>\n");
-                out->put("<pre>" + url + "</pre>\n");
-
-                out->flip();
-
-                session.write(out);
-                return;
-            }
-        }
-
-        /* request is NOT okay */
-        else
-        {
-            out->put("HTTP/1.1 400 BAD REQUEST\r\n");
-            out->put("Content-Type: text/html\r\n");
-            out->put("Connection: close\r\n");
-            out->put("\r\n");
-
-            out->put("<h1>400 BAD REQUEST</h1>\n");
-
-            out->flip();
-
-            session.write(out);
-            return;
-        }
+        // Write Response
+        session.write(res);
     }
 
-    void messageSent(Nwg::Session &session, Nwg::MessageBuffer &obj)
+    void messageSent(Nwg::Session &session, Nwg::MessageBuffer &msg)
     {
-        Nwg::MessageBuffer &msg = GETMSG(obj);
-        SessionState &state  = GETSTATE(session);
-
-        if (state.readAndWriteFile && state.nwritten < state.length)
-        {
-            /* continue reading file and sending it chunk by chunk @READBUFFSIZE */
-
-            std::shared_ptr<Nwg::MessageBuffer> out(new Nwg::MessageBuffer(BUFFSIZE));
-
-            std::ifstream *is = state.is;
-            char *buff = new char[READBUFFSIZE];
-
-            is->read(buff, READBUFFSIZE);
-            out->put(buff, is->gcount());
-            out->flip();
-
-            delete [] buff;
-
-            state.nwritten += is->gcount();
-            session.write(out);
-
-            if (state.nwritten == state.length) {
-                state.is->close();
-                delete state.is;
-                state.is = nullptr;
-
-                state.readAndWriteFile = false;
-                state.length = 0;
-                state.nwritten = 0;
-
-                _printf("==== STREAM CLOSED ====\n");
-            }
-        }
-
-        /* there is nothing left to be written */
-        else
-        {
-            session.close();
-
-            _printf("==== DATA IS COMPLETELY SENT ====\n");
-        }
+        session.close();
+        _printf("|||| RESPONSE SENT #%d\n", session.get<int>("req_no"));
     }
 
     void sessionClosed(Nwg::Session &session)
     {
-        SessionState &state = GETSTATE(session);
-
-        if (state.readAndWriteFile) {
-            state.is->close();
-            delete state.is;
-            state.is = nullptr;
-
-            _printf("==== STREAM CLOSED ====\n");
-            _printf("==== DATA IS NOT COMPLETELY SENT ====\n");
-        }
+        _printf("|||| SESSION CLOSED #%d\n", session.get<int>("req_no"));
     }
 };
 
-boost::regex HttpHandler::pattern = boost::regex(PATTERN);
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Main
+////////////////////////////////////////////////////////////////////////////////////////////////////
 void run(int port)
 {
     Nwg::EventLoop eventLoop;
@@ -283,7 +200,7 @@ void run(int port)
 
     acceptor.setBuffSize(BUFFSIZE);
     acceptor.setReadBuffSize(READBUFFSIZE);
-    acceptor.setProtocolCodec(std::make_shared<Nwg::BasicProtocolCodec>());
+    acceptor.setProtocolCodec(std::make_shared<HttpProtocolCodec>());
     acceptor.setHandler(std::make_shared<HttpHandler>());
 
     printf("Listening on port %d\n", acceptor.getPort());
@@ -295,8 +212,6 @@ void run(int port)
 
 int main(int argc, char **argv)
 {
-    workingPath = absolute(current_path());
-
     run([&]() -> int {
         if (argc > 1) {
             return std::stoi(argv[1]);
